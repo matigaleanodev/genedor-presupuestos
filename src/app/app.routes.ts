46b69f02fbafff904c7520c7833ccdfb8d610efd
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2024-04-18 10:35:41 -0300

    componentes para footer y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'form',
     loadComponent: () =>
       import('./components/doc-form/doc-form.component').then(
         (c) => c.DocFormComponent
       ),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'home',
   },
 ];
